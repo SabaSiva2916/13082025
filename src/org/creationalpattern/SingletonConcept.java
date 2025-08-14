3c4647cd4c6e853dc4492f139d5359a16978477f
Sabasiva2916 <sabasiva1629@gmail.com>
2025-08-14 12:55:51 +0530

    test works

--- src/org/creationalpattern/SingletonConcept.java
+++ src/org/creationalpattern/SingletonConcept.java
@@ -16,4 +16,14 @@ public class SingletonConcept {
 		return singeton;
 
 	}
+	
+	public void reverseOfString() {
+		String str = "Java Program";
+		String output = "";
+		
+		for(int i=str.length()-1; i>=0; i--) {
+			output = output+str.charAt(i);
+		}
+		System.out.println("Reverse of String :"+ output);
+	}
 }
